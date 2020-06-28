8e4ab695c6b9fb6c0bfb95764580959e1062b045
Matthieu Dartiguenave <matthieu@localhost.localdomain>
2020-06-28 12:32:58 +0100

    Suppress edited model

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,7 +2,47 @@
 
 package model
 
+type Node interface {
+	IsNode()
+}
+
 type EditNoteInput struct {
 	Title *string `json:"title"`
 	Text  *string `json:"text"`
+}
+
+type Note struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+func (Note) IsNode() {}
+
+type PageInfo struct {
+	HasNextPage     bool    `json:"hasNextPage"`
+	HasPreviousPage bool    `json:"hasPreviousPage"`
+	StartCursor     *string `json:"startCursor"`
+	EndCursor       *string `json:"endCursor"`
+}
+
+type User struct {
+	ID              string               `json:"id"`
+	Name            string               `json:"name"`
+	Email           string               `json:"email"`
+	Node            Node                 `json:"node"`
+	NotesConnection *UserNotesConnection `json:"notesConnection"`
+}
+
+func (User) IsNode() {}
+
+type UserNoteEdge struct {
+	Cursor string `json:"cursor"`
+	Node   *Note  `json:"node"`
+}
+
+type UserNotesConnection struct {
+	Edges      []*UserNoteEdge `json:"edges"`
+	PageInfo   *PageInfo       `json:"pageInfo"`
+	TotalCount int             `json:"totalCount"`
 }
